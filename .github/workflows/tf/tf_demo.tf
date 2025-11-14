7bd5b25fe3e5b24ccc7a8b650f473259ea84954a
Deepakthreat <97612169+Deepakthreat@users.noreply.github.com>
2025-11-14 15:04:28 +0530

    Update tf_demo.tf

--- .github/workflows/tf/tf_demo.tf
+++ .github/workflows/tf/tf_demo.tf
@@ -1,75 +1,46 @@
-### Providers
-#OneMSCIUX_Non-Prod
-provider "azurerm" {
-    tenant_id       = "7a9376d4-7c43-480f-82ba-a090647f651d"  #MSCI OFFICE 365
-    subscription_id = "a0bfe367-8299-4495-bec6-59d9688f95d2"  #OneMSCIUX_Non-Prod
-    features {}
+resource "google_service_account" "default" {
+  account_id   = "service_account_id"
+  display_name = "Service Account"
 }
 
-#SSPROD
-provider "azurerm" {
-    tenant_id       = "7a9376d4-7c43-480f-82ba-a090647f651d"  #MSCI OFFICE 365
-    subscription_id = "2382f82d-348c-426c-a1ec-a64503468ae5"  #SharedServices_Prod
-    alias           = "SharedServices_Prod"
-    features {}
-}
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
 
-#Security_Prod
-variable "sentinel" {
-    type = map(string)
-        default = {
-            "rgrp"              = "rgrp-pva2-sm-securitylogs"
-            "lanw"              = "lanw-pva2-sm-logs001"
-            "subscription_id"   = "a891da20-a4f6-42e4-83eb-fa852bf698f6"
-            "tenant_id"         = "7a9376d4-7c43-480f-82ba-a090647f651d"
-    }
-}
-provider "azurerm" {
-    alias           = "Security_Prod"
-    subscription_id = var.sentinel["subscription_id"]
-    tenant_id       = var.sentinel["tenant_id"]
-    features {}
-}
+  tags = ["foo", "bar"]
 
-#AzureAD
-provider "azuread" {
-  tenant_id       = "7a9376d4-7c43-480f-82ba-a090647f651d"  #MSCI OFFICE 365
-}
-
-#TF Version
-terraform {
-  required_version = "1.0.11"
-}
-
-#Provider Versions
-terraform {
-  required_providers {
-
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "2.88.1"
-    }
-
-    azuread = {
-      source = "hashicorp/azuread"
-      version = "2.12.0"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
     }
   }
-}
 
-#Backend
-terraform {
-  backend "azurerm" {
-    tenant_id               = "7a9376d4-7c43-480f-82ba-a090647f651d"  #MSCI OFFICE 365
-    subscription_id         = "a0bfe367-8299-4495-bec6-59d9688f95d2"  #OneMSCIUX_Non-Prod
-    resource_group_name     = "rgrp-dva2-ux-tfstate"  #East US 2 RGRP
-    storage_account_name    = "saccdva2uxtfstate001"
-    container_name          = "tfstate-ie1-infrastructure"
-    key                     = "ux-nonprod-ie1-infrastructure.terraform.tfstate"
-    snapshot                = true
+  // Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
   }
-}
 
-#Client Config Current
-data "azurerm_client_config" "current" {
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = google_service_account.default.email
+    scopes = ["cloud-platform"]
+  }
 }
